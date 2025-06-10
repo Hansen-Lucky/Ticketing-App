4f9b53056f24275808824d19ec283e28511d4d7c
Hansen-Lucky <hansenluckygunawan@smkwikrama.sch.id>
2025-06-10 09:51:58 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/date_symbol_data_local.dart'; // Tambahkan ini
+import 'package:intl/date_symbol_data_local.dart'; 
 import 'package:ticket_app/firebase_options.dart';
 import 'package:ticket_app/page/first_ticket.dart';
 
@@ -11,7 +11,7 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  // ✅ Tambahkan inisialisasi ini
+
   await initializeDateFormatting('id_ID', null);
 
   runApp(const MainApp());
